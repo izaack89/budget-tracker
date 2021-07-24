168e960ae36486352a61908627f54fe642807edf
German Ramirez <mitsuominagi@gmail.com>
2021-07-24 11:48:01 -0500

    Added code on the service-worker of the files to be on the cache

--- service-worker.js
+++ service-worker.js
@@ -2,5 +2,11 @@ const CACHE_NAME = "budget-tracker-cache-v1";
 const DATA_CACHE_NAME = "budget-data-cache-v1";
 
 let urlToCache = [
-
+    "/",
+    "db.js",
+    "index.js",
+    "manifest.json",
+    "style.css",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
 ];
