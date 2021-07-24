48952f3457e75f1b774900f12862251922fe1cde
German Ramirez <mitsuominagi@gmail.com>
2021-07-24 11:56:17 -0500

    Added the code for the fetch

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "budget-tracker-cache-v1";
 const DATA_CACHE_NAME = "budget-data-cache-v1";
 
-let urlToCache = [
+const FILES_TO_CACHE = [
     "/",
     "/db.js",
     "/index.js",
@@ -13,9 +13,42 @@ let urlToCache = [
 
 self.addEventListener("install",function(evt){
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache){
+        caches.open(CACHE_NAME).then(cache => {
             console.log("Cache");
-            return cache.addAll(urlToCache);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  });
